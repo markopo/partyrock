81df8cc4b753828bc7d1fae4c2b0dd964027211f
Marko Poikkimäki <markopoikkimaki@markos-mbp.lan>
2017-07-31 07:37:17 +0200

    Initial Commit

---
+++ PartyRockApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Marko Poikkimäki on 2017-07-31.
+//  Copyright © 2017 Marko Poikkimäki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
